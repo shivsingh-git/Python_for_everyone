f44f981e1703ce4412030343ea28c3d4dac9b0c3
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-04-22 09:07:27 +0530

    Update CalculatorPP.py

--- CalculatorPP.py
+++ CalculatorPP.py
@@ -3,6 +3,12 @@ def calculator(a,b,operator):
         return(a+b)
     elif operator == 'subtract':
         return(a-b)
+    elif operator == 'multiply':
+        return(a*b)
+    elif operator == 'devide':
+        return(a/b)
+    elif operator == 'modulo':
+        return(a%b)
     else :
         print("Error choose your word wisely")    
 
