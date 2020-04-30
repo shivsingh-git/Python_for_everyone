a0c415435d08800ab7eaabee8570c74108468601
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-04-30 20:24:31 +0530

    Update ASCI.py

--- ASCI.py
+++ ASCI.py
@@ -2,15 +2,16 @@
 #every even character with the character having just lower ascii code. Print the value returned.
 #Sample input: abcg
 #Sample output: badf
+
 s=input("Enter the String")
-l=""
-j=0
+l=""                                            #creating the new list
+j=0                                             #initializing the new variable
 x=len(s)
-for i in range(1,x+1):
-    if((i%2)==0):
-        l+=(chr(ord(s[j])-1))
-        j+=1
+for i in range(1,x+1):                          #runnig the loop from 1 to len to get the position
+    if((i%2)==0):                               #checking if the position is odd or not
+        l+=(chr(ord(s[j])-1))                   #frst convrting the char to asci,then adding 1 and finally converting it to chr 
+        j+=1                                    #incrementing the value of j
     elif((i%2)!=0):
         l+=(chr(ord(s[j])+1))
         j+=1
-print(l)       
+print(l)                                        #printing the list
