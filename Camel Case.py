cb6d978cdc31830c704c0247410e2ec59499b43f
Shiv Pratap Singh <pratapsinghshiv898@gmail.com>
2020-05-01 20:31:34 +0530

    Update Camel Case.py

--- Camel Case.py
+++ Camel Case.py
@@ -7,13 +7,13 @@
 #Competitive
 #Programmer
 s=input("Enter The Camel String: ")                         #taking the String as a Input
-l=''                
+l=''                                                        #initializing the new string
 l=s[0]                                                      #assigning the frst letter of the string to list l
 for i in range(1,len(s)):
-    if (s[i].isupper()==True) :                             #if the parser incounters the capital letter 
-        print(l)                                            #then print the list
+    if (s[i].isupper()==True) :                             #if the parser incounters the capital letter print the list 
+        print(l)                                         
         l=''                                                #making the list empty again
         l=s[i]                                              #assigning the capital letter to the new string
     else:
-        l=l+s[i]                                            #assinging all the lower case strings to the list
+        l=l+s[i]                                            #putting all the lower case strings in the list
 print(l)                                                    #to print the last string after the last capital letter according to my program
